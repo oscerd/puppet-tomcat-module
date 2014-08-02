32185407c702a6695bc6516693782096de181736
ancosen <ancosen@gmail.com>
2014-08-02 12:06:29 +0200

    Added local and web mode

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -2,6 +2,7 @@ define tomcat::setup (
   $family = undef,
   $update_version = undef,
   $extension = undef,
+  $mode = undef,
   $tmpdir = undef,
   $installdir = undef
   ) { 
@@ -20,11 +21,8 @@ define tomcat::setup (
     fail('Extension parameter must be set')
   }
   
-  if ($tmpdir == undef){
-    notify{'Temp folder not specified, setting default install folder /tmp/':}
-    $defined_tmpdir ='/tmp/'
-  } else {
-    $defined_tmpdir = $tmpdir
+  if ($mode == undef) {
+    fail('mode parameter must be set')
   }
   
   # Validate parameters  
@@ -37,27 +35,62 @@ define tomcat::setup (
     fail('Extension parameter must be ".tar.gz" or "zip"')
   }
   
+  if (($mode != 'web') and ($mode != 'local')) {
+    fail('mode parameter must have value "local" or "web"')
+  }
+  
+  if ($installdir == undef){
+    notify{'Install folder not specified, setting default install folder /opt/':}
+    $defined_installdir ='/opt/'
+  } else {
+    $defined_installdir = $installdir
+  }
+  
+  if ($tmpdir == undef){
+    notify{'Temp folder not specified, setting default install folder /tmp/':}
+    $defined_tmpdir ='/tmp/'
+  } else {
+    $defined_tmpdir = $tmpdir
+  }
+  
   $tomcat = "apache-tomcat"
   
+  if ($mode == "local"){
   file { "${defined_tmpdir}${tomcat}-${family}.0.${update_version}${extension}":
 		      ensure => present,
 		      source => "puppet:///modules/tomcat/${tomcat}-${family}.0.${update_version}${extension}" }
-
-  exec { 'extract_tomcat': 
+  
+    exec { 'extract_tomcat': 
           command => "tar -xzvf ${defined_tmpdir}${tomcat}-${family}.0.${update_version}${extension} -C ${defined_tmpdir}",
           require => [ File[ "${defined_tmpdir}${tomcat}-${family}.0.${update_version}${extension}"], 
                        Package['tar'] ], 
-          alias => extract_tomcat }
-                       
-  file { "$installdir":
+          alias => extract_tomcat } 
+  }
+  elsif ($mode == "web"){
+    
+  $source = "http://apache.fastbull.org/tomcat/tomcat-7/v7.0.55/bin/${tomcat}-${family}.0.${update_version}${extension}"
+
+  exec { 'retrieve_tomcat': 
+          command => "wget -q ${source} -P ${defined_tmpdir}",
+          unless => "ls ${defined_installdir}${tomcat}-${family}.0.${update_version}/",
+          timeout => 1000 }    
+          
+    exec { 'extract_tomcat': 
+          command => "tar -xzvf ${defined_tmpdir}${tomcat}-${family}.0.${update_version}${extension} -C ${defined_tmpdir}",
+          require => [ Exec[ 'retrieve_tomcat'], 
+                       Package['tar'] ], 
+          alias => extract_tomcat } 
+  } 
+                     
+  file { "$defined_installdir":
 		      ensure => directory,
 		      mode => '755',
 		      owner => 'root', 
 		      alias => tomcat_home }
   
   exec { 'move_tomcat': 
-          command => "mv ${defined_tmpdir}${tomcat}-${family}.0.${update_version}/ ${installdir}",
+          command => "mv ${defined_tmpdir}${tomcat}-${family}.0.${update_version}/ ${defined_installdir}",
           require => [ File[ tomcat_home ], 
                        Exec[ extract_tomcat ] ],
-                    unless => "ls ${installdir}${tomcat}-${family}.0.${update_version}/" }
+                    unless => "ls ${defined_installdir}${tomcat}-${family}.0.${update_version}/" }
 }
