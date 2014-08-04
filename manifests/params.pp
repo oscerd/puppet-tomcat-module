e0c780ecb017341ab04361b0a3d610f54fc38c62
Paolo Gigante <paolo.gigante@postecom.it>
2014-08-04 18:20:21 +0200

    Added shutdown port in the params

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,6 +8,7 @@ class tomcat::params{
   $http_port = "8082"
   $https_port = "8083"
   $ajp_port = "8007"
+  $shutdown_port = "8001"
   $http_connection_timeout = "20000"
   $https_max_threads = "150"
 }
