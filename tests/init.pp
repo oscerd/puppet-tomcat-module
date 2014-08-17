bcaba0143c914a8c16719f21f1eebb5333ad3c47
ancosen <ancosen@gmail.com>
2014-08-17 15:35:27 +0200

    Update documentation

--- tests/init.pp
+++ tests/init.pp
@@ -1,5 +1,6 @@
 # Just copy init.pp in your manifests directory. Place the tomcat package into /tomcat/files folder, if you select "local" mode.
-# The module extract the folder and move it in a specified path
+# The module extract the folder and move it in a specified path. Copy sample.war into /tomcat/files folder. The sample.war package is the Apache example 
+# from this url https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/ 
 
 # Resource Default for exec
 Exec {
@@ -27,6 +28,8 @@ tomcat::setup { "tomcat":
 
 tomcat::deploy { "deploy":
   war_name => "sample",
+  war_versioned => "no",
+  war_version => "",
   deploy_path => "/webapps/",
   family => "7",
   update_version => "55",
